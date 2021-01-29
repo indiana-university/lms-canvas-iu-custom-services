28e5bc3f4aab9789b6642219087aff6d01e75098
chmaurer <chmaurer@iu.edu>
2021-01-29 16:37:05 -0500

    Converting photoroster to a microservice Adding required stuff to canvas service Adding required stuff to iu-only (suds) service

---
+++ lms-iuonly-impl/src/main/java/lms/iuonly/model/SudsClass.java
@@ -0,0 +1,17 @@
+package lms.iuonly.model;
+
+import lombok.Data;
+
+/**
+ * Created by chmaurer on 1/21/15.
+ */
+@Data
+public class SudsClass {
+
+    private String courseId;
+    private String courseOfferNumber;
+    private String sTerm;
+    private String institution;
+    private String classNumber;
+
+}
---
+++ lms-iuonly-impl/src/main/java/lms/iuonly/model/SudsFerpaEntry.java
@@ -0,0 +1,15 @@
+package lms.iuonly.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SudsFerpaEntry implements Serializable {
+    private String ferpa;
+    private String iuImsUsername;
+}
