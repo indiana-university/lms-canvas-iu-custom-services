28f588c1c8b9e64776fafe8fcc86389b3a681e5b
chmaurer <chmaurer@iu.edu>
2021-02-04 16:24:54 -0500

    LMSA-7283 Migrating the redirect tool Also needed the RoleResolver and VariableReplacementService Also needed an anonymous version of the suds service

--- lms-iuonly-client/src/main/java/iuonly/config/IuOnlyClientConfig.java
+++ lms-iuonly-client/src/main/java/iuonly/config/IuOnlyClientConfig.java
@@ -1,12 +1,14 @@
 package iuonly.config;
 
 import edu.iu.uits.lms.common.oauth.OAuthConfig;
+import edu.iu.uits.lms.common.oauth.OpenResourceOwnerPasswordResourceDetails;
 import iuonly.client.generated.ApiClient;
 import iuonly.client.generated.api.FeatureAccessApi;
 import iuonly.client.generated.api.SudsApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.token.AccessTokenRequest;
@@ -30,11 +32,24 @@ public class IuOnlyClientConfig {
         return new FeatureAccessApi(apiClient());
     }
 
+    /**
+     * This is the primary bean that will be used under normal circumstances
+     * @return
+     */
     @Bean
+    @Primary
     public SudsApi sudsApi() {
         return new SudsApi(apiClient());
     }
 
+    /**
+     * This is an alternative bean that can be used in cases where there was no authentication (open endpoint)
+     * @return
+     */
+    @Bean(name = "sudsApiViaAnonymous")
+    public SudsApi sudsApiViaAnonymous() {
+        return new SudsApi(apiClientViaAnonymous());
+    }
 
     private ApiClient apiClient() {
         if (apiClient == null) {
@@ -44,9 +59,35 @@ public class IuOnlyClientConfig {
         return apiClient;
     }
 
+    private ApiClient apiClientViaAnonymous() {
+        if (apiClient == null) {
+            apiClient = new ApiClient(iuOnlyClientRestTemplateViaAnonymous());
+            apiClient.setBasePath(baseServiceUrl);
+        }
+        return apiClient;
+    }
+
     @Bean(name = "iuOnlyClientRestTemplate")
     public OAuth2RestTemplate iuOnlyClientRestTemplate() {
         ResourceOwnerPasswordResourceDetails resourceDetails = new ResourceOwnerPasswordResourceDetails();
+        resourceDetails.setClientId(oAuthConfig.getClientId());
+        resourceDetails.setClientSecret(oAuthConfig.getClientSecret());
+        resourceDetails.setUsername(oAuthConfig.getClientId());
+        resourceDetails.setPassword(oAuthConfig.getClientPassword());
+        resourceDetails.setAccessTokenUri(oAuthConfig.getAccessTokenUri());
+        resourceDetails.setClientAuthenticationScheme(AuthenticationScheme.form);
+        resourceDetails.setScope(Arrays.asList("iusvcs:read", "iusvcs:write"));
+
+        AccessTokenRequest atr = new DefaultAccessTokenRequest();
+        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext(atr);
+
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resourceDetails, clientContext);
+        return restTemplate;
+    }
+
+    @Bean(name = "iuOnlyClientRestTemplateViaAnonymous")
+    public OAuth2RestTemplate iuOnlyClientRestTemplateViaAnonymous() {
+        OpenResourceOwnerPasswordResourceDetails resourceDetails = new OpenResourceOwnerPasswordResourceDetails();
         resourceDetails.setClientId(oAuthConfig.getClientId());
         resourceDetails.setClientSecret(oAuthConfig.getClientSecret());
         resourceDetails.setUsername(oAuthConfig.getClientId());
