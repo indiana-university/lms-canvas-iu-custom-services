2aadf70126ae019810aadade3d4345b2c20d3975
chmaurer <chmaurer@iu.edu>
2021-05-19 14:57:47 -0400

    LMSA-7461 Move the batch email stuff from dept prov to iuonly Add an email summary job for regular provisioning stuff in batch

---
+++ lms-iuonly-impl/src/main/java/lms/iuonly/services/BatchEmailServiceImpl.java
@@ -0,0 +1,68 @@
+package lms.iuonly.services;
+
+import io.swagger.annotations.Api;
+import lms.iuonly.model.LmsBatchEmail;
+import lms.iuonly.repository.LmsBatchEmailRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by yingwang on 10/29/15.
+ */
+@RestController
+@RequestMapping("/batchemail")
+@Slf4j
+@Api(tags = "batchEmail")
+public class BatchEmailServiceImpl {
+
+    @Autowired
+    private LmsBatchEmailRepository lmsBatchEmailRepository;
+
+    @GetMapping(value = "/{id}")
+    public LmsBatchEmail getBatchEmailFromId(@PathVariable Long id) {
+        return lmsBatchEmailRepository.findById(id).orElse(null);
+    }
+
+    @GetMapping(value = "/groupcode/{groupcode}")
+    public LmsBatchEmail getBatchEmailFromGroupCode(@PathVariable String groupcode) {
+        return lmsBatchEmailRepository.getBatchEmailFromGroupCode(groupcode);
+    }
+
+    @GetMapping(value = "/all")
+    public List<LmsBatchEmail> getBatchEmailAll() {
+        List<LmsBatchEmail> batchEmails = (List<LmsBatchEmail>) lmsBatchEmailRepository.findAll();
+        return batchEmails;
+    }
+
+    @PutMapping(value = "/{id}")
+    public LmsBatchEmail updateBatchEmail(@PathVariable Long id, @RequestBody String emails) {
+        LmsBatchEmail updatedBatchEmail = lmsBatchEmailRepository.findById(id).orElse(null);
+        updatedBatchEmail.setEmails(emails);
+        return lmsBatchEmailRepository.save(updatedBatchEmail);
+    }
+
+    @PostMapping("/")
+    public LmsBatchEmail createBatchEmail(@RequestBody LmsBatchEmail lmsBatchEmail) {
+        LmsBatchEmail newBatchEmail = new LmsBatchEmail();
+        newBatchEmail.setGroupCode(lmsBatchEmail.getGroupCode());
+        newBatchEmail.setEmails(lmsBatchEmail.getEmails());
+        return lmsBatchEmailRepository.save(newBatchEmail);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public String deleteBatchEmail(@PathVariable Long id) {
+        lmsBatchEmailRepository.deleteById(id);
+        return "Delete success.";
+    }
+
+}
